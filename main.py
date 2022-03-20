ecaa918c96e97ab9689b6d4c09583578ac577f1a
mircoloo <bismirco@gmail.com>
2022-03-20 21:19:16 +0100

    mac update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!python3
+from time import sleep
+import click
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
@@ -10,36 +12,65 @@ cap.set(4,720)
 detector = HandDetector(detectionCon=0.8)
 keys = [["Q","W","E","R","T","Y","U","I","O","P"],
          ["A","S","D","F","G","H","J","K","L"],
-         ["Z","X","C","V","B","N","M"]]
+         ["Z","X","C","V","B","N","M",";","."]]
+finalText = ""
+clicked = False
 
+def draw_all(img, button_list):
+    
+    for button in button_list:
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(img, button.pos , (x+w,y+h),(0,0,255), cv2.FILLED)
+        cv2.putText(img, button.text,(x+5,y+25), 
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3)
+
+    return img
 
 class Button():
-    def __init__(self, pos, text, size=[85,85]) -> None:
+    def __init__(self, pos, text, size=[40,40]) -> None:
         self.pos = pos
         self.text = text
         self.size = size
-        x, y = self.pos
-        w, h = self.size
-        cv2.rectangle(img, self.pos , (x+w,y+h),(0,0,255))
-        cv2.putText(img, self.text,(x+20,y+65), 
-                    cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 4)
+        
         
 
 buttonList = []
-
-
-
+for i in range(len(keys)): 
+        for j,key in enumerate(keys[i]):
+            buttonList.append(Button([60*j+30,60*i+30], key))
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bboxInfo = detector.findPosition(img)
-
-    for x,key in enumerate(keys[0]):
-        buttonList.append(Button([100*x+50,50], key))
+    img = draw_all(img, buttonList)
     
-    
+    if lmList:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
 
+            if x < lmList[8][0] < x+w and y < lmList[8][1] < y +h:
+                cv2.rectangle(img, button.pos , (x+w,y+h),(0,0,175), cv2.FILLED)
+                cv2.putText(img, button.text,(x+5,y+25), 
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3)
+
+                l,_,_ = detector.findDistance(8,12, img, draw=False)
+
+
+                if l <= 50 and not clicked:
+                    clicked = True
+                    cv2.rectangle(img, button.pos , (x+w,y+h),(0,255,0), cv2.FILLED)
+                    cv2.putText(img, button.text,(x+5,y+25), 
+                            cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3)
+                    finalText += button.text
+                if l > 60:
+                    clicked = False
+                    
+    
+    cv2.rectangle(img, (50,250) , (600, 450),(100,100,255), cv2.FILLED)
+    cv2.putText(img, finalText,(60,425), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
